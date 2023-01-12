ab767005ec7044e372f1b364b5532978060c8dbc
tabris1971 <johnholmes8@me.com>
2023-01-12 16:41:42 +0200

    Last commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
 //test
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int i) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$i.wav'));
+  }
+
+  Expanded buildKey({Color keyColour, int noteNumber}) {
+    return Expanded(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: keyColour,
+        ),
+        onPressed: () {
+          playSound(noteNumber);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [buildKey(keyColour: Colors.red, noteNumber: 1),
+            buildKey(keyColour: Colors.orange, noteNumber: 2),
+            buildKey(keyColour: Colors.yellow, noteNumber: 3),
+            buildKey(keyColour: Colors.green, noteNumber: 4),
+            buildKey(keyColour: Colors.blue, noteNumber: 5),
+            buildKey(keyColour: Colors.indigo, noteNumber: 6),
+            buildKey(keyColour: Colors.purple, noteNumber: 7)],
+          ),
         ),
       ),
     );
